0dbc89e67bfc1eb8524b42010f62d4c16fc7ff8a
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-09-15 15:24:49 +0100

    defining a recursive function

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 # addition
 def add(n1, n2):
     return n1 + n2
@@ -28,14 +20,27 @@ operations = {
     "/" : divide,
     "*" : multiply
 }
-continue_calulating False
-while not continue_calculating:
-numb1 = float(input("what's the first number?: "))
-for operator in operations:
-    print(operator)
-operation_symbol = input("pick an operation from the line above: ")
-numb2 = float(input("what's the second number?: "))
 
-calculation_function = operations[operation_symbol]
-answer = calculation_function(numb1, numb2)
-print(f"{numb1} {operation_symbol} {numb2} = {answer}")
+# this calculator function is a recursive function that calls it self 
+# it takes no input and has no output.
+def calculator():
+    numb1 = float(input("what's the first number?: "))
+    for operator in operations:
+        print(operator)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("pick an operation: ")
+        numb2 = float(input("what's the next number?: "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(numb1, numb2)
+        print(f"{numb1} {operation_symbol} {numb2} = {answer}")
+        if input(f"type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            numb1 = answer
+        else:
+            should_continue = False
+            # this calculator call will take us back to the beginning
+            calculator()
+
+calculator()
+        
