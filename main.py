8129c0e5cc60cd2d7cab3e74f909ffb13e3ab27a
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-09-15 12:03:14 +0100

    getting inputs

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
+
+
+
+
+
+
+
+
+# addition
 def add(n1, n2):
-    return n1+n2
+    return n1 + n2
+
+# subtraction
+def subtract(n1, n2):
+    return n1 - n2
+
+# division
+def divide(n1, n2):
+    return n1 / n2
+
+# multiplication
+def multiply(n1, n2):
+    return n1 * n2
+# dictionary to be able to access each operation
+operatioons = {
+    "+" : add,
+    "-" : subtract,
+    "/" : divide,
+    "*" : multiply
+}
+
+numb1 = float(input("what's the first number? :"))
+numb2 = float(input("what's the second number? :"))
